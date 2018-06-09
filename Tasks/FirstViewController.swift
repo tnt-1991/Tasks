37ed0f68332fac945f55c94af9eb67a363a2c4f6
Kirill Klimovich <tnt@kirills-macbook-air.local>
2018-06-09 12:42:37 +0200

    Initial Commit

---
+++ Tasks/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tasks
+//
+//  Created by Kirill Klimovich on 09/06/2018.
+//  Copyright © 2018 Kirill Klimovich. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tasks/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tasks
+//
+//  Created by Kirill Klimovich on 09/06/2018.
+//  Copyright © 2018 Kirill Klimovich. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
